9200e16911f6d924db6780109f5b9a7cdc1f25fe
Steven Davis <stevendavis@Steven-Daviss-MacBook-Air-2.local>
2011-02-10 21:28:02 -0600

    Pulling Ross' changes.

--- gen/com/sbasite/sbasites/R.java
+++ gen/com/sbasite/sbasites/R.java
@@ -40,11 +40,7 @@ public final class R {
         public static final int Button06=0x7f060008;
         public static final int Button07=0x7f060007;
         public static final int Button08=0x7f060009;
-<<<<<<< HEAD
-        public static final int Button10=0x7f060024;
-=======
-        public static final int Button10=0x7f06001e;
->>>>>>> f712168280151a1fee747fce070d574adc47ec92
+        public static final int Button10=0x7f060027;
         public static final int CheckBox01=0x7f06000a;
         public static final int CheckBox02=0x7f06000b;
         public static final int CheckBox03=0x7f06000c;
@@ -54,67 +50,43 @@ public final class R {
         public static final int SiteNameTextView=0x7f060011;
         public static final int TextView01=0x7f060006;
         public static final int TextView02=0x7f060004;
-<<<<<<< HEAD
-        public static final int TextView03=0x7f060025;
-        public static final int button1=0x7f060012;
-        public static final int button2=0x7f060015;
-        public static final int button3=0x7f060017;
-        public static final int button4=0x7f06001a;
-        public static final int button5=0x7f06001d;
-        public static final int button6=0x7f060020;
-        public static final int button7=0x7f060022;
-        public static final int instructions=0x7f060028;
-        public static final int layers=0x7f060026;
-        public static final int list_view=0x7f060027;
-        public static final int map=0x7f060010;
-        public static final int searchText=0x7f06000f;
-        public static final int textView1=0x7f060011;
-        public static final int textView10=0x7f06001f;
-        public static final int textView11=0x7f060021;
-        public static final int textView12=0x7f060023;
-        public static final int textView2=0x7f060013;
-        public static final int textView3=0x7f060014;
-        public static final int textView4=0x7f060016;
-        public static final int textView5=0x7f060018;
-        public static final int textView6=0x7f060019;
-        public static final int textView7=0x7f06001b;
-        public static final int textView8=0x7f06001c;
-        public static final int textView9=0x7f06001e;
-=======
-        public static final int TextView03=0x7f06001f;
+        public static final int TextView03=0x7f060028;
         public static final int TopLayout=0x7f06000f;
-        public static final int button1=0x7f060017;
-        public static final int button2=0x7f060016;
+        public static final int button1=0x7f060015;
+        public static final int button2=0x7f060018;
         public static final int button3=0x7f06001a;
-        public static final int button4=0x7f06001c;
-        public static final int instructions=0x7f060022;
-        public static final int layers=0x7f060020;
-        public static final int list_view=0x7f060021;
+        public static final int button4=0x7f06001d;
+        public static final int button5=0x7f060020;
+        public static final int button6=0x7f060023;
+        public static final int button7=0x7f060025;
+        public static final int instructions=0x7f06002b;
+        public static final int layers=0x7f060029;
+        public static final int list_view=0x7f06002a;
         public static final int map=0x7f060013;
         public static final int searchText=0x7f060012;
-        public static final int textView1=0x7f060015;
-        public static final int textView10=0x7f06001d;
-        public static final int textView2=0x7f060014;
-        public static final int textView5=0x7f060018;
-        public static final int textView6=0x7f060019;
-        public static final int textView8=0x7f06001b;
->>>>>>> f712168280151a1fee747fce070d574adc47ec92
+        public static final int textView1=0x7f060014;
+        public static final int textView10=0x7f060022;
+        public static final int textView11=0x7f060024;
+        public static final int textView12=0x7f060026;
+        public static final int textView2=0x7f060016;
+        public static final int textView3=0x7f060017;
+        public static final int textView4=0x7f060019;
+        public static final int textView5=0x7f06001b;
+        public static final int textView6=0x7f06001c;
+        public static final int textView7=0x7f06001e;
+        public static final int textView8=0x7f06001f;
+        public static final int textView9=0x7f060021;
     }
     public static final class layout {
         public static final int instructions=0x7f030000;
         public static final int layers=0x7f030001;
         public static final int listview=0x7f030002;
         public static final int main=0x7f030003;
-<<<<<<< HEAD
         public static final int searchaddress=0x7f030004;
         public static final int searchcoordinates=0x7f030005;
         public static final int searchsitenameaddress=0x7f030006;
-=======
-        public static final int searchcoordinates=0x7f030004;
-        public static final int searchsitenameaddress=0x7f030005;
-        public static final int site_list_item=0x7f030006;
->>>>>>> f712168280151a1fee747fce070d574adc47ec92
-        public static final int sitedetailactivity=0x7f030007;
+        public static final int site_list_item=0x7f030007;
+        public static final int sitedetailactivity=0x7f030008;
     }
     public static final class menu {
         public static final int menu=0x7f050000;
@@ -123,15 +95,15 @@ public final class R {
         public static final int about=0x7f040006;
         public static final int about_sba_communications=0x7f040005;
         public static final int address=0x7f040008;
-        public static final int address_exact_search=0x7f04002e;
-        public static final int address_partial_search=0x7f040031;
-        public static final int address_search_with_full_address=0x7f04002c;
+        public static final int address_exact_search=0x7f040030;
+        public static final int address_partial_search=0x7f040033;
+        public static final int address_search_with_full_address=0x7f04002e;
         public static final int app_name=0x7f040019;
         public static final int app_version=0x7f040004;
         public static final int back=0x7f04000b;
         public static final int canada=0x7f040010;
         public static final int central_america=0x7f04000c;
-        public static final int city_state_province=0x7f04002f;
+        public static final int city_state_province=0x7f040031;
         public static final int coordinate_pair_example=0x7f040026;
         public static final int coordinates=0x7f040007;
         public static final int current=0x7f040017;
@@ -141,19 +113,19 @@ public final class R {
         public static final int done=0x7f04000a;
         public static final int email_support=0x7f040000;
         public static final int enter_site=0x7f040016;
-        public static final int ex_1430_beechwood=0x7f04002d;
-        public static final int ex_33487=0x7f040036;
+        public static final int ex_1430_beechwood=0x7f04002f;
+        public static final int ex_33487=0x7f040038;
         public static final int ex_53_5_117_2=0x7f040027;
         public static final int ex_53_5_117_2b=0x7f04002a;
-        public static final int ex_boca_raton=0x7f040030;
-        public static final int ex_california=0x7f040034;
+        public static final int ex_boca_raton=0x7f040032;
+        public static final int ex_california=0x7f040036;
         public static final int ex_fl=0x7f040020;
         public static final int ex_fl98463_l=0x7f040022;
         public static final int ex_hackberry=0x7f04001d;
-        public static final int ex_lawrenceville_ga=0x7f040032;
-        public static final int ex_miami=0x7f040038;
+        public static final int ex_lawrenceville_ga=0x7f040034;
+        public static final int ex_miami=0x7f04003a;
         public static final int ex_shadow=0x7f04001b;
-        public static final int ex_vancouver=0x7f040039;
+        public static final int ex_vancouver=0x7f04003b;
         public static final int full_site_name_search=0x7f04001e;
         public static final int information1=0x7f040018;
         public static final int instructions=0x7f040013;
@@ -162,7 +134,7 @@ public final class R {
         public static final int layers=0x7f040012;
         public static final int list_view=0x7f040011;
         public static final int longitude_without_accepted=0x7f040029;
-        public static final int major_city=0x7f040037;
+        public static final int major_city=0x7f040039;
         public static final int managed=0x7f04000d;
         public static final int new_construction=0x7f04000f;
         public static final int owned=0x7f04000e;
@@ -175,13 +147,10 @@ public final class R {
         public static final int site_name=0x7f04002c;
         public static final int site_name_examples=0x7f04001a;
         public static final int site_name_id=0x7f040009;
-        public static final int state_province=0x7f040033;
+        public static final int state_province=0x7f040035;
         public static final int support=0x7f040001;
         public static final int symbol_is_not_required_for_long=0x7f04002b;
-<<<<<<< HEAD
-        public static final int zip_code=0x7f040035;
-=======
         public static final int userLocationClicked=0x7f04002d;
->>>>>>> f712168280151a1fee747fce070d574adc47ec92
+        public static final int zip_code=0x7f040037;
     }
 }
